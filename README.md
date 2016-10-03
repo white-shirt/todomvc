41dd0fd72581a697a8115e2f88cb0a9b9b239be4
mlefree <mat@mlefree.com>
2016-10-03 13:42:48 +0200

    add service auth

--- README.md
+++
@@ -1,3 +0,0 @@
-# todomvc
-# git clone
-# npm install
---
+++ src/app/auth.service.ts
@@ -0,0 +1,27 @@
+
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthService {
+
+
+  //mail : string
+  //password : string
+
+  //constructor() {
+//
+  //  console.log('MyAuthService constructor')
+  //}
+
+
+  getHeroes(): void {} // stub
+
+  /*login(mail : string, password : string) : void {
+    //todo MLE implement
+    console.log('MyAuthService.login('+mail+','+password+')');
+    this.mail = mail;
+    this.password = password;
+  }*/
+}
+
+
